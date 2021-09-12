8b9ac196852003e91d514e34d02b31f5fd3250cf
Simanta Barua <simanta.barua@yhaoo.com>
2021-09-13 01:44:15 +0600

    fix bug

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
+  // const url = `https://fakestoreapi.com/products`;
+  const url = "products.json"
   fetch(url)
     .then((response) => response.json())
     .then((data) => showProducts(data));
@@ -30,8 +31,8 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
